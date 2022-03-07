bd8b01a95a4b3437fa16523a21aab057a33f8e2d
Mona Lisa <diligaf68@noreply.github.com>
2022-03-07 22:20:06 +0000

    About Complex List

---
+++ mix-list/animal-list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+    # create a list called animals
+    animals = ["Fox", "Fly", " Ant", " Bee", " Cod", " Cat", " Dog", " Yak", " Cow", "Hen", " Koi", " Hog". " Jay", " Kit"]
+    
+    # display list1
+    print(animals)
+
+main()
